0a50603a45f0451fc520f479067ca765b53cdbd7
Abdelrhman <abdelr7mane7ab@gmail.com>
2025-11-04 11:01:45 +0200

    updated

--- src/components/Herbas/Herbas.jsx
+++ src/components/Herbas/Herbas.jsx
@@ -1,7 +1,318 @@
 import React, { useEffect, useState } from 'react'
+import OurVision1 from "../../assets/OurVision.jpg";
+import OurVision2 from "../../assets/OurVision2.jpg";
+import OurVision3 from "../../assets/pexels-mareefe-672046.jpg";
 
 export default function Herbas() {
   return (
-    <div>Herbas</div>
+    <>
+      <div className="container-fluid second-nav px-lg-5 px-md-4 px-3">
+        <div className="categories mb-2 pt-2">
+          <ul className='list-unstyled d-flex gap-1 overflow-x-auto flex-nowrap'>
+            <li className='pointer cat'>Adaptogens</li>
+            <li className='pointer cat'>Anti-inflammatory herbs</li>
+            <li className='pointer cat'>Antioxidant herbs</li>
+            <li className='pointer cat'>Antimicrobial / Antiseptic herbs</li>
+            <li className='pointer cat'>Digestive herbs</li>
+            <li className='pointer cat'>Detoxifying herbs</li>
+            <li className='pointer cat'>Calming / Sedative herbs</li>
+            <li className='pointer cat'>Stimulant herbs</li>
+            <li className='pointer cat'>Expectorant herbs</li>
+            <li className='pointer cat'>Immunomodulating herbs</li>
+          </ul>
+        </div>
+      </div>
+      <div className="container-fluid px-lg-5 px-md-4 px-3 my-4">
+        <div className="herbas row g-3">
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision1} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision2} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision3} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision3} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision1} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision3} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision3} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision1} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision3} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision1} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision2} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision1} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision2} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision3} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision3} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision1} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision2} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision3} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision3} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision1} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision1} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision2} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision1} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision2} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision3} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision3} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision1} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision2} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision1} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision2} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision3} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision3} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision1} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision2} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision1} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision2} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision3} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision3} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision1} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision2} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+          <div className="col-6 col-sm-4 col-md-3 col-xxl-2">
+            <div className="col-content">
+              <img className='w-100 rounded-2' src={OurVision1} alt="" />
+              <p className='aboutIcone'>category</p>
+              <p>name</p>
+            </div>
+          </div>
+        </div>
+      </div >
+    </>
   )
 }
--- src/components/Navbar/NavbarComponent.jsx
+++ src/components/Navbar/NavbarComponent.jsx
@@ -5,30 +5,46 @@ import Navbar from 'react-bootstrap/Navbar';
 import { IoNotifications } from "react-icons/io5";
 import { FaCartShopping } from "react-icons/fa6";
 import { FaUserCircle } from "react-icons/fa";
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { GiHerbsBundle } from "react-icons/gi";
+import { MdDarkMode } from "react-icons/md";
+import profile from '../../assets/profile.jpg'
 
 export default function NavbarComponent() {
+  const [profileAppear, setProfileAppear] = useState(false)
   return (
-    <div className='container-fluid  px-md-5 px-3 position-sticky top-0 bg-white shadow py-1'>
-      <Navbar collapseOnSelect expand="lg">
-        <NavLink to={'/'} className='fw-bold fs-3 logo'><GiHerbsBundle className='active fs-1' /> HerbalCare</NavLink>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mx-auto mt-1 d-flex gap-4 fw-medium">
-            <NavLink to={'/'}>Home</NavLink>
-            <NavLink to={'herbas'}>Herbas</NavLink>
-            <NavLink to={'activity'}>Activity</NavLink>
-            <NavLink to={'features'}>AI Features</NavLink>
-            <NavLink to={'ContactUs'}>Contact Us</NavLink>
-          </Nav>
-          <Nav className='d-flex gap-4 flex-row align-items-center'>
-            <span className='navIcon'><IoNotifications /></span>
-            <NavLink to="cart" className='navIcon'><FaCartShopping /></NavLink>
-            <span className='navIcon'><FaUserCircle /></span>
-          </Nav>
-        </Navbar.Collapse>
-    </Navbar >
-    </div>
+    <>
+      <div className='container-fluid px-md-5 px-3 position-sticky top-0 bg-white shadow py-1 z-1'>
+        <Navbar collapseOnSelect expand="lg">
+          <NavLink to={'/'} className='fw-bold fs-3 logo'><GiHerbsBundle className='active fs-1' /> HerbalCare</NavLink>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto mt-1 d-flex gap-4 fw-medium">
+              <NavLink className={'cat'} to={'/'}>Home</NavLink>
+              <NavLink className={'cat'} to={'herbas'}>Herbas</NavLink>
+              <NavLink className={'cat'} to={'activity'}>Activity</NavLink>
+              <NavLink className={'cat'} to={'features'}>AI Features</NavLink>
+              <NavLink className={'cat'} to={'ContactUs'}>Contact Us</NavLink>
+            </Nav>
+            <Nav className='d-flex gap-4 flex-row align-items-center position-relative z-0'>
+              <MdDarkMode className='fs-5 pointer' />
+              <IoNotifications className='fs-5 pointer' />
+              <FaCartShopping className='fs-5 pointer' />
+              <FaUserCircle onClick={()=>{profileAppear?setProfileAppear(false):setProfileAppear(true)}} className='fs-5 pointer' />
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar >
+        <div className={`p-5 profile-dropdown ${profileAppear?'profile-dropdown-appear':'profile-dropdown-disappear'}`}>
+          <img className='profile-image' src={profile} alt="" />
+            <p className='text-white'>welcome back!</p>
+          <div className='d-flex gap-2'>
+            <button className='btn btn-success'><Link className='text-white' to={'/register'} onClick={()=>{setProfileAppear(false)}}>Register</Link></button>
+            <button className='btn btn-success'><Link className='text-white' to={'/login'} onClick={()=>{setProfileAppear(false)}}>Login</Link></button>
+          </div>
+        </div>
+      </div>
+
+    </>
+
   );
 }
